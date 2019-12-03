19d9ff70fd90da391ff386c2ec91633183721a81
Kyriakos Chatzidimitriou <kyrcha@gmail.com>
2019-12-03 15:23:57 +0200

    Change basic routing

--- back-end/app.js
+++ back-end/app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
 if(process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../front-end/build/index.html'))
   })
+} else {
+  app.get('/', (req, res) => res.send('Hello World!'))
 }
 
 module.exports = app
