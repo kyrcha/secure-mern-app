3644d39a69a23e53106920d3760095dbe2b67f7e
Kyriakos Chatzidimitriou <kyrcha@gmail.com>
2019-12-03 15:58:48 +0200

    Add static folder

--- back-end/app.js
+++ back-end/app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const app = express()
 
 if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../front-end/build/')))
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../front-end/build/index.html'))
   })
